f2eb5831f3213c6b142c5225d075ac141ad6aa20
Luke Go <sangch.go@gmail.com>
2019-11-18 17:36:52 +0900

    odroidThings: weather_board
    Signed-off-by: Luke Go <sangch.go@gmail.com>

--- app/src/main/java/odroid/hardkernel/com/thingsgpioexample/MainActivity.java
+++ app/src/main/java/odroid/hardkernel/com/thingsgpioexample/MainActivity.java
@@ -2,9 +2,9 @@ package odroid.hardkernel.com.thingsgpioexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Switch;
-
+import android.util.Log;
+import com.google.android.things.pio.GpioCallback;
+import com.google.android.things.pio.I2cDevice;
 import com.google.android.things.pio.PeripheralManager;
 import com.google.android.things.pio.Gpio;
 
@@ -25,36 +25,64 @@ public class MainActivity extends AppCompatActivity {
 
         // get available gpio pin list.
         // each pin name is consist as P + physical pin number.
-        List<String> gpioList = manager.getGpioList();
+        List<String> gpioList = manager.getI2cBusList();
 
-        try {
-            // get first available gpio pin.
-            // in this case, Physical pin #3 is used.
-            gpio = manager.openGpio(gpioList.get(0));
+        for(String i2c: gpioList)
+            Log.d("things", "i2c - " + i2c);
 
-            // set the pin's direction and initial state.
-            gpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
-            Switch gpioSwitch = findViewById(R.id.gpio_switch);
+        try (I2cDevice i2c = manager.openI2cDevice(gpioList.get(0), 0x60)) {
+            byte id = i2c.readRegByte(0);
+            Log.d("i2c things", "id - " + id);
+            if (id == 0x32) {
+                byte[] ucoef = {0x7b, 0x6b, 0x01, 0x00};
+                i2c.writeRegBuffer(0x13, ucoef, 4);
 
-            gpioSwitch.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    try {
-                        Switch gpioSwitch = (Switch) v;
-                        if (gpioSwitch.isChecked()) {
-                            // set pin #3 to high, or 1.
-                            gpio.setValue(true);
-                        } else {
-                            // set pin #3 to low, or 0.
-                            gpio.setValue(false);
-                        }
-                    } catch (IOException io) {
-                        io.printStackTrace();
-                    }
+                byte chlist = (byte)(0x80|0x20|0x10);
+                writeParam(i2c, (byte)0x01, chlist);
+
+                i2c.writeRegByte(0x03, (byte) 0x01);
+                i2c.writeRegByte(0x04, (byte)0x01);
+
+                writeParam(i2c,(byte)0x0e, (byte) 0x00);
+                Thread.sleep(10);
+                writeParam(i2c, (byte)0x1e, (byte)0);
+                Thread.sleep(10);
+                writeParam(i2c, (byte)0x1d, (byte)0x70);
+                writeParam(i2c, (byte)0x1F, (byte)0x20);
+                Thread.sleep(10);
+                writeParam(i2c,(byte)0x1e, (byte)0);
+                Thread.sleep(10);
+                writeParam(i2c, (byte)0x1d, (byte)0x70);
+                writeParam(i2c, (byte)0x12, (byte)0x20);
+                Thread.sleep(10);
+                i2c.writeRegByte(0x08, (byte)0xff);
+                i2c.writeRegByte(0x18,(byte)0x0E);
+                while(true) {
+                    Log.d("i2c things", "UV_idx - " + (readUV(i2c) / 100.0));
+                    Log.d("i2c things", "visiable -" + readVisible(i2c) + " lux");
+                    Log.d("i2c things", "IR - " + readIR(i2c) + " lux");
+                    Thread.sleep(1000);
                 }
-            });
-        } catch (Exception exception) {
-            exception.printStackTrace();
-        }
+            }
+        } catch (Exception e) {}
+    }
+    void writeParam(I2cDevice device, byte param, byte val) {
+        try {
+            device.writeRegByte(0x17, val);
+            device.writeRegByte(0x18, (byte)(param | (byte)0xA0));
+        } catch (IOException e) {}
+    }
+
+    short readUV(I2cDevice device) throws Exception {
+        Thread.sleep(10);
+        return device.readRegWord(0x2c);
+    }
+    double readVisible(I2cDevice device) throws Exception {
+        Thread.sleep(10);
+        return ((device.readRegWord(0x22) - 256)/0.282) * 14.5;
+    }
+    double readIR(I2cDevice device) throws Exception {
+        Thread.sleep(10);
+        return ((device.readRegWord(0x24) - 250) / 2.44) * 14.5;
     }
 }
