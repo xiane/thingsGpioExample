19e744554a805993752b10972dfe312962f7b674
Luke Go <sangch.go@gmail.com>
2019-11-19 12:16:55 +0900

    odroidThings: 16x2: Lcd test
    Signed-off-by: Luke Go <sangch.go@gmail.com>

--- app/src/main/java/odroid/hardkernel/com/thingsgpioexample/MainActivity.java
+++ app/src/main/java/odroid/hardkernel/com/thingsgpioexample/MainActivity.java
@@ -2,19 +2,23 @@ package odroid.hardkernel.com.thingsgpioexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import com.google.android.things.pio.GpioCallback;
 import com.google.android.things.pio.I2cDevice;
 import com.google.android.things.pio.PeripheralManager;
-import com.google.android.things.pio.Gpio;
 
-import java.io.IOException;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
     PeripheralManager manager;
-    Gpio gpio;
+    final static int I2C_ADDR = 0x27;
+    final static int LCD_WIDTH = 16;
+    final static byte LCD_CHR = 1;
+    final static byte LCD_CMD = 0;
+    final static byte LCD_LINE_1 = (byte) 0x80;
+    final static byte LCD_LINE_2 = (byte) 0xC0;
+    final static byte LCD_BACKLIGHT = 0x08;
+    final static byte ENABLE = 0b00000100;
+
+    I2cDevice device;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,66 +27,49 @@ public class MainActivity extends AppCompatActivity {
         // get Peripheral Manager for managing the gpio.
         manager = PeripheralManager.getInstance();
 
-        // get available gpio pin list.
-        // each pin name is consist as P + physical pin number.
-        List<String> gpioList = manager.getI2cBusList();
+        List<String> i2cBusList = manager.getI2cBusList();
 
-        for(String i2c: gpioList)
-            Log.d("things", "i2c - " + i2c);
+        try (I2cDevice i2c = manager.openI2cDevice(i2cBusList.get(0), I2C_ADDR)) {
+            device = i2c;
 
-        try (I2cDevice i2c = manager.openI2cDevice(gpioList.get(0), 0x60)) {
-            byte id = i2c.readRegByte(0);
-            Log.d("i2c things", "id - " + id);
-            if (id == 0x32) {
-                byte[] ucoef = {0x7b, 0x6b, 0x01, 0x00};
-                i2c.writeRegBuffer(0x13, ucoef, 4);
+            lcd_byte(0x33, LCD_CMD);
+            lcd_byte(0x32, LCD_CMD);
+            lcd_byte(0x06, LCD_CMD);
+            lcd_byte(0x0C, LCD_CMD);
+            lcd_byte(0x28, LCD_CMD);
+            lcd_byte(0x01, LCD_CMD);
+            Thread.sleep(5);
 
-                byte chlist = (byte)(0x80|0x20|0x10);
-                writeParam(i2c, (byte)0x01, chlist);
+            while (true) {
+                lcd_string("***ODROID-N2*** ", LCD_LINE_1);
+                lcd_string("ODROID-magazine ", LCD_LINE_2);
+                Thread.sleep(3000);
 
-                i2c.writeRegByte(0x03, (byte) 0x01);
-                i2c.writeRegByte(0x04, (byte)0x01);
-
-                writeParam(i2c,(byte)0x0e, (byte) 0x00);
-                Thread.sleep(10);
-                writeParam(i2c, (byte)0x1e, (byte)0);
-                Thread.sleep(10);
-                writeParam(i2c, (byte)0x1d, (byte)0x70);
-                writeParam(i2c, (byte)0x1F, (byte)0x20);
-                Thread.sleep(10);
-                writeParam(i2c,(byte)0x1e, (byte)0);
-                Thread.sleep(10);
-                writeParam(i2c, (byte)0x1d, (byte)0x70);
-                writeParam(i2c, (byte)0x12, (byte)0x20);
-                Thread.sleep(10);
-                i2c.writeRegByte(0x08, (byte)0xff);
-                i2c.writeRegByte(0x18,(byte)0x0E);
-                while(true) {
-                    Log.d("i2c things", "UV_idx - " + (readUV(i2c) / 100.0));
-                    Log.d("i2c things", "visiable -" + readVisible(i2c) + " lux");
-                    Log.d("i2c things", "IR - " + readIR(i2c) + " lux");
-                    Thread.sleep(1000);
-                }
+                lcd_string("***HardKernel***", LCD_LINE_1);
+                lcd_string("*hardkernel.com*", LCD_LINE_2);
+                Thread.sleep(3000);
             }
         } catch (Exception e) {}
     }
-    void writeParam(I2cDevice device, byte param, byte val) {
-        try {
-            device.writeRegByte(0x17, val);
-            device.writeRegByte(0x18, (byte)(param | (byte)0xA0));
-        } catch (IOException e) {}
+
+    void lcd_byte(int bits, byte mode) throws Exception {
+        byte bits_high = (byte) (mode | (bits & 0xF0) | LCD_BACKLIGHT);
+        byte bits_low = (byte) (mode | (bits <<4) & 0xF0 | LCD_BACKLIGHT);
+        device.writeRegByte(0, bits_high);
+        lcd_toogle_enable(bits_high);
+        device.writeRegByte(0, bits_low);
+        lcd_toogle_enable(bits_low);
     }
 
-    short readUV(I2cDevice device) throws Exception {
-        Thread.sleep(10);
-        return device.readRegWord(0x2c);
+    void lcd_toogle_enable(byte bits) throws Exception {
+        device.writeRegByte(0, (byte) (bits|ENABLE));
+        device.writeRegByte(0, (byte) (bits& ~ENABLE));
     }
-    double readVisible(I2cDevice device) throws Exception {
-        Thread.sleep(10);
-        return ((device.readRegWord(0x22) - 256)/0.282) * 14.5;
-    }
-    double readIR(I2cDevice device) throws Exception {
-        Thread.sleep(10);
-        return ((device.readRegWord(0x24) - 250) / 2.44) * 14.5;
+
+    void lcd_string(String msg, byte line) throws Exception {
+        lcd_byte(line, LCD_CMD);
+        for(char ch: msg.toCharArray()) {
+            lcd_byte(ch, LCD_CHR);
+        }
     }
 }
