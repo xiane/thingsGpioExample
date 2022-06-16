9c2f33647a95b45d9360bf5d7f87be64622a2366
Luke go <sangch.go@gmail.com>
2022-06-16 16:47:19 +0900

    odroidThings: Uart: Add text view for IO.
    - Add a text view for reading received message.
    - Add a text view and send button for transfer the message.
    
    Signed-off-by: Luke go <sangch.go@gmail.com>

--- app/src/main/java/odroid/hardkernel/com/thingsgpioexample/MainActivity.java
+++ app/src/main/java/odroid/hardkernel/com/thingsgpioexample/MainActivity.java
@@ -4,19 +4,27 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Switch;
+import android.widget.TextView;
 
 import com.google.android.things.pio.PeripheralManager;
 import com.google.android.things.pio.UartDevice;
 import com.google.android.things.pio.UartDeviceCallback;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    final String TAG = "UartExample";
     PeripheralManager manager;
     UartDevice uart;
     UartDeviceCallback callback;
@@ -24,10 +32,42 @@ public class MainActivity extends AppCompatActivity {
     private HandlerThread uartBackGround;
     private Handler uartHandelr;
 
+    private TextView uart_rx_txt;
+    private EditText uart_tx_txt;
+    private Button uart_send_btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        uart_rx_txt = findViewById(R.id.uart_msg_txt);
+        uart_tx_txt = findViewById(R.id.uart_send_txt);
+        uart_send_btn = findViewById(R.id.uart_send_btn);
+
+        uart_send_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Editable msg = uart_tx_txt.getText();
+                try {
+                    byte[] msgByte = msg.toString().getBytes("UTF-8");
+                    Log.d(TAG, Arrays.toString(msgByte));
+                    uart.write(msgByte, msgByte.length);
+
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        uart_tx_txt.setText("");
+                    }
+                });
+            }
+        });
 
         uartBackGround = new HandlerThread("UartBackground");
         uartBackGround.start();
@@ -55,15 +95,26 @@ public class MainActivity extends AppCompatActivity {
             callback = new UartDeviceCallback() {
                 @Override
                 public boolean onUartDeviceDataAvailable(UartDevice uartDevice) {
-                    byte[] buff = new byte[20];
+                    final byte[] buff = new byte[20];
                     try {
                         int length = uartDevice.read(buff, 20);
 
                         if (length > 0) {
-                            String retString = new String(buff, 0, length);
+                            for(int i=0; i<length; i++) {
+                                Log.d("UartExample", String.format("[%d] %d\n", i, buff[i]));
+                            }
+                            final String retString = new String(buff, 0, length, "UTF-8");
                             Log.d("UartExample", "msg - " + retString);
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    uart_rx_txt.setText(retString);
+                                }
+                            });
                         }
                     } catch (IOException e) {
+                        e.printStackTrace();
+                    } catch (Exception e) {
                         e.printStackTrace();
                     }
                     return true;
