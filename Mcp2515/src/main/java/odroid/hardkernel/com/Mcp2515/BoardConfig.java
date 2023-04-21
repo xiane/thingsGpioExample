e3cac7fc6536a0d58ce481eb0765b39f4dfe4de0
Luke go <sangch.go@gmail.com>
2023-04-21 18:11:38 +0900

    Mcp2515: Add Mcp2515 example project.
    Signed-off-by: Luke go <sangch.go@gmail.com>

---
+++ Mcp2515/src/main/java/odroid/hardkernel/com/Mcp2515/BoardConfig.java
@@ -0,0 +1,6 @@
+package odroid.hardkernel.com.Mcp2515;
+
+public class BoardConfig {
+    static public final String MCP2515_SPI = "SPI0.0";
+    static public final String MCP2515_INT = "11";
+}
---
+++ Mcp2515/src/main/java/odroid/hardkernel/com/Mcp2515/MainActivity.java
@@ -0,0 +1,55 @@
+package odroid.hardkernel.com.Mcp2515;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.hardkernel.odroid.things.contrib.Mcp2515.Mcp2515;
+
+import android.hardkernel.com.Mcp2515.R;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+
+public class MainActivity extends AppCompatActivity {
+    private Mcp2515 mcp2515;
+    private TextView can_msg_txt;
+    private final Mcp2515.MessageReceivedListener canMsgListener = message -> {
+        StringBuilder canMsg = new StringBuilder();
+        canMsg.append(String.format("id - 0x%02x\n", message.getId()));
+        ByteBuffer buffer = message.getData();
+        for (int i = 0; i < message.getDataLength(); i++)
+            canMsg.append(String.format("data[%d] - 0x%02x\n", i, (byte) buffer.get(i)));
+
+        can_msg_txt.setText(canMsg.toString());
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        can_msg_txt = findViewById(R.id.can_msg_txt);
+
+        try {
+            mcp2515 = new Mcp2515(BoardConfig.MCP2515_SPI, BoardConfig.MCP2515_INT);
+
+            mcp2515.setListener(canMsgListener);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Button testSendBtn = findViewById(R.id.test_send);
+        testSendBtn.setOnClickListener(v -> mcp2515.sendTest());
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        mcp2515.close();
+    }
+
+}
