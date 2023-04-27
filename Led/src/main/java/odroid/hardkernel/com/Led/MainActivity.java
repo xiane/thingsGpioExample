07e20d6c3f4835139035ee28571b192e0d86beb1
Luke go <sangch.go@gmail.com>
2023-04-27 17:27:52 +0900

    Led: Add led example project.
    Signed-off-by: Luke go <sangch.go@gmail.com>

---
+++ Led/src/main/java/odroid/hardkernel/com/Led/MainActivity.java
@@ -0,0 +1,35 @@
+package odroid.hardkernel.com.Led;
+
+import android.hardkernel.com.Led.R;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Switch;
+
+import com.hardkernel.odroid.things.contrib.Led.Led;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+    private boolean ledOn = false;
+    private Led led;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        try {
+            led = new Led("7");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Switch LED = findViewById(R.id.led_switch);
+        LED.setOnClickListener(v -> {
+            ledOn = !ledOn;
+            try {
+                led.turn(ledOn);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+}
