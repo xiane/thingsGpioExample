c51455987b32f13c1366e89f7de97f26cddf3889
Luke go <sangch.go@gmail.com>
2023-08-21 12:13:06 +0900

    Pwm: Changed whole UI and way to use.
    - Now, you can control all of available pwm on the 40pin connector.
    
    Signed-off-by: Luke go <sangch.go@gmail.com>

--- Pwm/src/main/java/odroid/hardkernel/com/Pwm/MainActivity.java
+++ Pwm/src/main/java/odroid/hardkernel/com/Pwm/MainActivity.java
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SwitchCompat;
 
 import android.util.Log;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.SeekBar;
 
 import com.google.android.things.pio.PeripheralManager;
@@ -23,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        LinearLayout pwmListLayout = findViewById(R.id.pwm_list);
         // get a Peripheral Manager for managing the pwm.
         manager = PeripheralManager.getInstance();
 
@@ -30,45 +33,62 @@ public class MainActivity extends AppCompatActivity {
         // each pwm name is physical pin number.
         List<String> pwmList = manager.getPwmList();
 
-        for(String pin:pwmList)
+        for(String pin:pwmList) {
             Log.d("things", "pwm name - " + pin);
-        SwitchCompat pwmSwitch = findViewById(R.id.pwm_switch);
-        try {
-            final Pwm pwm = manager.openPwm(pwmList.get(0));
-
-            pwm.setPwmFrequencyHz(3000);
-            SeekBar pwm_dutyCycle = findViewById(R.id.pwm_dutyCycle_seekbar);
-
-            pwm_dutyCycle.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-                @Override
-                public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                    try {
-                        pwm.setPwmDutyCycle(progress);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-
-                @Override
-                public void onStartTrackingTouch(SeekBar seekBar) {
-
-                }
-
-                @Override
-                public void onStopTrackingTouch(SeekBar seekBar) {
-
-                }
-            });
-            pwmSwitch.setOnClickListener(v -> {
-                try {
-                    SwitchCompat gpioSwitch = (SwitchCompat) v;
-                    pwm.setEnabled(gpioSwitch.isChecked());
-                } catch (IOException io) {
-                    io.printStackTrace();
-                }
-            });
-        } catch (Exception e) {
-            e.printStackTrace();
+            try {
+                Pwm pwm = manager.openPwm(pin);
+                pwm.setPwmFrequencyHz(3000);
+                LinearLayout pwmLayout = buildPwmLayout(pwm);
+                pwmListLayout.addView(pwmLayout);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
+    }
+
+    private LinearLayout buildPwmLayout(Pwm pwm) {
+        LinearLayout pwmLinear = new LinearLayout(this);
+        LinearLayout.LayoutParams layoutParams =
+                new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT);
+        int margin = 20;
+        layoutParams.setMargins(margin, margin, margin, margin);
+
+        pwmLinear.setLayoutParams(layoutParams);
+
+        pwmLinear.setOrientation(LinearLayout.HORIZONTAL);
+
+        SwitchCompat pwmSwitch = new SwitchCompat(this);
+        pwmSwitch.setText(pwm.getName());
+        pwmSwitch.setOnCheckedChangeListener((compoundButton, b) -> {
+            try {
+                pwm.setEnabled(b);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        pwmLinear.addView(pwmSwitch);
+
+        SeekBar pwmDutyCycle = new SeekBar(this);
+        pwmDutyCycle.setLayoutParams(new ViewGroup.LayoutParams(600,50));
+        pwmDutyCycle.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                try {
+                    pwm.setPwmDutyCycle(i);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
+        pwmLinear.addView(pwmDutyCycle);
+
+        return pwmLinear;
     }
 }
